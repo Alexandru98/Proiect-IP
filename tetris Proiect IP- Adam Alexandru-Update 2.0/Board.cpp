66153b62a67f4a5be39685e89df45952ad0ce01f
Alexandru98 <adam.alexr98@gmail.com>
2017-01-13 15:26:34 +0200

    Add files via upload

---
+++ tetris Proiect IP- Adam Alexandru-Update 2.0/Board.cpp
@@ -0,0 +1,146 @@
+#ifndef _BOARD_
+#define _BOARD_
+#ifndef _PIECES_
+#define _PIECES_
+class Pieces
+{
+public:
+
+	int GetBlockType		(int pPiece, int pRotation, int pX, int pY);
+	int GetXInitialPosition (int pPiece, int pRotation);
+	int GetYInitialPosition (int pPiece, int pRotation);
+};
+
+#endif
+
+#define BOARD_LINE_WIDTH 6
+#define BLOCK_SIZE 16
+#define BOARD_POSITION 320
+#define BOARD_WIDTH 10
+#define BOARD_HEIGHT 20
+#define MIN_VERTICAL_MARGIN 20
+#define MIN_HORIZONTAL_MARGIN 20
+#define PIECE_BLOCKS 5
+
+class Board
+{
+public:
+
+	Board						(Pieces *pPieces, int pScreenHeight);
+
+	int GetXPosInPixels			(int pPos);
+	int GetYPosInPixels			(int pPos);
+	bool IsFreeBlock			(int pX, int pY);
+	bool IsPossibleMovement		(int pX, int pY, int pPiece, int pRotation);
+	void StorePiece				(int pX, int pY, int pPiece, int pRotation);
+	void DeletePossibleLines	();
+	bool IsGameOver				();
+
+private:
+
+	enum { POS_FREE, POS_FILLED };
+	int mBoard [BOARD_WIDTH][BOARD_HEIGHT];
+	Pieces *mPieces;
+	int mScreenHeight;
+
+	void InitBoard();
+	void DeleteLine (int pY);
+};
+
+#endif
+
+Board::Board (Pieces *pPieces, int pScreenHeight)
+{
+	mScreenHeight = pScreenHeight;
+	mPieces = pPieces;
+	InitBoard();
+}
+void Board::InitBoard()
+{
+	for (int i = 0; i < BOARD_WIDTH; i++)
+		for (int j = 0; j < BOARD_HEIGHT; j++)
+			mBoard[i][j] = POS_FREE;
+}
+
+void Board::StorePiece (int pX, int pY, int pPiece, int pRotation)
+{
+	for (int i1 = pX, i2 = 0; i1 < pX + PIECE_BLOCKS; i1++, i2++)
+	{
+		for (int j1 = pY, j2 = 0; j1 < pY + PIECE_BLOCKS; j1++, j2++)
+		{
+			if (mPieces->GetBlockType (pPiece, pRotation, j2, i2) != 0)
+				mBoard[i1][j1] = POS_FILLED;
+		}
+	}
+}
+
+bool Board::IsGameOver()
+{
+	for (int i = 0; i < BOARD_WIDTH; i++)
+	{
+		if (mBoard[i][0] == POS_FILLED) return true;
+	}
+
+	return false;
+}
+
+void Board::DeleteLine (int pY)
+{
+	for (int j = pY; j > 0; j--)
+	{
+		for (int i = 0; i < BOARD_WIDTH; i++)
+		{
+			mBoard[i][j] = mBoard[i][j-1];
+		}
+	}
+}
+
+void Board::DeletePossibleLines ()
+{
+	for (int j = 0; j < BOARD_HEIGHT; j++)
+	{
+		int i = 0;
+		while (i < BOARD_WIDTH)
+		{
+			if (mBoard[i][j] != POS_FILLED) break;
+			i++;
+		}
+
+		if (i == BOARD_WIDTH) DeleteLine (j);
+	}
+}
+bool Board::IsFreeBlock (int pX, int pY)
+{
+	if (mBoard [pX][pY] == POS_FREE) return true; else return false;
+}
+int Board::GetXPosInPixels (int pPos)
+{
+	return  ( ( BOARD_POSITION - (BLOCK_SIZE * (BOARD_WIDTH / 2)) ) + (pPos * BLOCK_SIZE) );
+}
+int Board::GetYPosInPixels (int pPos)
+{
+	return ( (mScreenHeight - (BLOCK_SIZE * BOARD_HEIGHT)) + (pPos * BLOCK_SIZE) );
+}
+bool Board::IsPossibleMovement (int pX, int pY, int pPiece, int pRotation)
+{
+	for (int i1 = pX, i2 = 0; i1 < pX + PIECE_BLOCKS; i1++, i2++)
+	{
+		for (int j1 = pY, j2 = 0; j1 < pY + PIECE_BLOCKS; j1++, j2++)
+		{
+			if (	i1 < 0 			||
+				i1 > BOARD_WIDTH  - 1	||
+				j1 > BOARD_HEIGHT - 1)
+			{
+				if (mPieces->GetBlockType (pPiece, pRotation, j2, i2) != 0)
+					return 0;
+			}
+			if (j1 >= 0)
+			{
+				if ((mPieces->GetBlockType (pPiece, pRotation, j2, i2) != 0) &&
+					(!IsFreeBlock (i1, j1))	)
+					return false;
+			}
+		}
+	}
+	return true;
+}
